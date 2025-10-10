8d715aa5b040ab8e46da9497f0bdf7669df56bc5
yuzvdp <dmitriy@yuzvyak.ru>
2025-10-10 11:44:22 +0300

    add board.domain.entities

---
+++ src/Domain/Board.Domain/Base/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Board.Domain.Base
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ src/Domain/Board.Domain/Entities/Advert.cs
@@ -0,0 +1,9 @@
+﻿using Board.Domain.Base;
+
+namespace Board.Domain.Entities
+{
+    public class Advert : EntityBase
+    {
+        public string? Title { get; set; }
+    }
+}
