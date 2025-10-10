be1be1b79585a26542bf88f53218913bcb4e28ec
yuzvdp <dmitriy@yuzvyak.ru>
2025-10-10 11:48:28 +0300

    add board.contracts.dtos

---
+++ src/Contracts/Board.Contracts/Adverts/AdvertDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Board.Contracts.Adverts
+{
+    public class AdvertDto
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? Title { get; set; }
+    }
+}
