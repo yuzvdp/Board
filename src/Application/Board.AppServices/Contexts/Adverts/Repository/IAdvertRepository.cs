70d8e1ae648f152f07f589e36f4c2f68f7c30717
yuzvdp <dmitriy@yuzvyak.ru>
2025-10-10 11:58:42 +0300

    add board.application.appservices

---
+++ src/Application/Board.AppServices/Contexts/Adverts/Repository/IAdvertRepository.cs
@@ -0,0 +1,9 @@
+﻿using Board.Contracts.Adverts;
+
+namespace Board.AppServices.Contexts.Adverts.Repository
+{
+    public interface IAdvertRepository
+    {
+        Task<AdvertDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Application/Board.AppServices/Contexts/Adverts/Services/AdvertService.cs
@@ -0,0 +1,13 @@
+﻿using Board.AppServices.Contexts.Adverts.Repository;
+using Board.Contracts.Adverts;
+
+namespace Board.AppServices.Contexts.Adverts.Services
+{
+    public class AdvertService(IAdvertRepository advertRepository) : IAdvertService
+    {
+        public Task<AdvertDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return advertRepository.GetByIdAsync(id, cancellationToken);
+        }
+    }
+}
