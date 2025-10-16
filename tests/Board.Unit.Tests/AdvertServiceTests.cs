4fddf786628db698f1bab258c0e166f1d840aee6
yuzvdp <dmitriy@yuzvyak.ru>
2025-10-16 17:01:58 +0300

    Важный комментарий

--- tests/Board.Unit.Tests/AdvertServiceTests.cs
+++ tests/Board.Unit.Tests/AdvertServiceTests.cs
@@ -11,7 +11,7 @@ namespace Board.Unit.Tests
         [Fact]
         public async Task GetByIdAsync_Should_Call_Repository()
         {
-            // Arrange Андреев
+            // Arrange Andreev
             Mock<IAdvertRepository> advertRepositoryMock = new();
             Mock<IMapper> mapperMock = new();
 
@@ -26,10 +26,10 @@ namespace Board.Unit.Tests
                 .Setup(x => x.GetByIdAsync(guid, token))
                 .ReturnsAsync(article);
 
-            // Act Алексей
+            // Act Aleksey
             var result = await service.GetByIdAsync(guid, token);
 
-            // Assert Александрович
+            // Assert Aleksandrovich
             Assert.NotNull(result);
             Assert.Equal(article, result);
             advertRepositoryMock.Verify(x => x.GetByIdAsync(guid, token), Times.Once);
