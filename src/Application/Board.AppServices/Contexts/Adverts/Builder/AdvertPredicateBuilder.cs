04211d25a86e3165755772dd3ce4db69b3a8fe4b
yuzvdp <dmitriy@yuzvyak.ru>
2025-10-18 11:09:40 +0300

    Add AdvertPredicateBuilder

---
+++ src/Application/Board.AppServices/Contexts/Adverts/Builder/AdvertPredicateBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Board.Domain.Entities;
+
+namespace Board.AppServices.Contexts.Adverts.Builder
+{
+    public class AdvertPredicateBuilder : IAdvertPredicateBuilder
+    {
+        private IQueryable<Advert>? query;
+
+        public IQueryable<Advert> Build()
+        {
+            return query;
+        }
+
+        public IAdvertPredicateBuilder OrderbyTitle()
+        {
+            query = query.OrderBy(x => x.Title);
+            return this;
+        }
+
+        public IAdvertPredicateBuilder Paginate(int pageNumber, int pageSize)
+        {
+            query = query.Take(pageNumber).Skip(pageNumber * pageSize);
+            return this;
+        }
+    }
+}
---
+++ src/Application/Board.AppServices/Contexts/Adverts/Builder/IAdvertPredicateBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Board.Domain.Entities;
+
+namespace Board.AppServices.Contexts.Adverts.Builder
+{
+    public interface IAdvertPredicateBuilder
+    {
+        IAdvertPredicateBuilder OrderbyTitle();
+        IAdvertPredicateBuilder Paginate(int pageNumber, int pageSize);
+        IQueryable<Advert> Build();
+    }
+}
